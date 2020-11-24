cca15512a5606bab6ffbd9d70a5eb993f0eccd99
Beatriz Felix <mbeatrizmfelix@gmail.com>
2020-11-23 22:32:01 -0300

    chore: initial config

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'default',
+  type: 'postgres',
+  host: `${process.env.DB_HOST}`,
+  port: `${process.env.DB_PORT}`,
+  database: `${process.env.DB_NAME}`,
+  username: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  entities: ['src/typeorm/models/*.ts'],
+  migrations: ['src/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: ['src/typeorm/migrations'],
+  },
+};
