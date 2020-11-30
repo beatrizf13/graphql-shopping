6f2f44b0448534c87e1e7bcd62e340a2b31b3351
Beatriz Felix <mbeatrizmfelix@gmail.com>
2020-11-30 01:07:51 -0300

    chore(web): move wrapper components

--- packages/web/src/components/Wrapper/index.tsx
+++ packages/web/src/components/Wrapper/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Footer from '../Footer';
+import Footer from './Footer';
 
-import Header from '../Header';
+import Header from './Header';
 
 const Wrapper: React.FC = ({ children }) => (
   <>
